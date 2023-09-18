0f71d8e5ab97a1322d0670f57ee31dbf6ae7aefa
Igor Zolotin <goldin7777@gmail.com>
2023-09-18 19:34:50 +0300

    reshil z13

---
+++ seminar2/zad13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Enter triple figure: ");
+string anyNum = Console.ReadLine();
+string anyNumText = Convert.ToString(anyNum);
+if (anyNumText.Length > 2){
+  Console.WriteLine("Third number: " + anyNumText[2]);
+}
+else {
+  Console.WriteLine(":Third number is lost");
+}
