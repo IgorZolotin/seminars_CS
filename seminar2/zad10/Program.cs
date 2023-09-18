57e602f124cc26cd13688ec2b83616bf659d8dd4
Igor Zolotin <goldin7777@gmail.com>
2023-09-18 19:28:40 +0300

    reshil z10

---
+++ seminar2/zad10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Write("Enter triple figure: ");
+string strNum = Console.ReadLine();
+int threeNum = int.Parse(strNum);
+strNum = Convert.ToString(threeNum);
+Console.WriteLine("second digit of the number: "+strNum[1]);
